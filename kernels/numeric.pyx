a5900ae791521adc0e9dc962c2713e92e8d91796
Radovan <radovan.horvat@gmail.com>
2020-11-04 16:34:43 +0100

    Implement numeric kernels - 2

--- kernels/numeric.pyx
+++ kernels/numeric.pyx
@@ -13,6 +13,8 @@ ctypedef np.float64_t DTYPE_t
 # Numerical kernels
 # ---------------------------------------------------
 
+# -------- old stuff
+
 @timing
 def calc_com_old(pos_vec, mass_vec):
     n, m = pos_vec.shape
@@ -21,6 +23,23 @@ def calc_com_old(pos_vec, mass_vec):
         center_of_mass[:, i] = pos_vec[:, i] * mass_vec
     return center_of_mass.sum(axis=0) / mass_vec.sum()
 
+
+@timing
+def calc_pe_old(pos_vec, mass_vec, epsilon, G=6.67e-11):
+    n, m = pos_vec.shape
+    interaction_matrix = np.zeros((n, n))
+    for i in range(n - 1):
+        R_1 = np.ones((n - (i + 1), m)) * pos_vec[i]
+        R_2 = pos_vec[i + 1:]
+        R = R_2 - R_1
+        interaction_matrix[i, i + 1:] = 1 / (np.linalg.norm(R, axis=1) + epsilon)
+    return -G * np.dot(interaction_matrix @ mass_vec, mass_vec)
+
+@timing
+def calc_ke_old(vel_vec, mass_vec):
+    return 0.5 * (vel_vec ** 2).sum(axis=1) @ mass_vec
+
+# -------- new stuff
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -44,6 +63,43 @@ cdef calc_com(DTYPE_t [:, :] r, DTYPE_t [:] m):
     return np.asarray(com)
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef calc_ke(DTYPE_t [:, :] v, DTYPE_t [:] m):
+    """
+        Calculate kinetic energy.
+    """
+    cdef int n = v.shape[0]
+    cdef int i
+    cdef double ke = 0
+
+    for i in range(n):
+        ke += m[i] * (v[i, 0] * v[i, 0] + v[i, 1] * v[i, 1] + v[i, 2] * v[i, 2])
+    return 0.5 * ke
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef calc_pe(DTYPE_t [:, :] r, DTYPE_t [:] m, double G, double eps):
+    """
+        Calculate potential energy.
+    """
+    cdef int n = r.shape[0]
+    cdef int i, j
+    cdef double dx, dy, dz, ds, d
+    cdef double pe = 0
+
+    for i in prange(n, nogil=True):
+        for j in range(i + 1, n):
+            dx = r[j, 0] - r[i, 0]
+            dy = r[j, 1] - r[i, 1]
+            dz = r[j, 2] - r[i, 2]
+            ds = dx * dx + dy * dy + dz * dz
+            d = sqrt(ds)
+            pe += m[i] * m[j] / (d + eps)
+    return - G * pe
+
+
 # ---------------------------------------------------
 # Wrappers
 # ---------------------------------------------------
@@ -51,3 +107,13 @@ cdef calc_com(DTYPE_t [:, :] r, DTYPE_t [:] m):
 @timing
 def calc_com_wrap(r, m):
     return calc_com(r, m)
+
+
+@timing
+def calc_ke_wrap(v, m):
+    return calc_ke(v, m)
+
+
+@timing
+def calc_pe_wrap(r, m, G, eps):
+    return calc_pe(r, m, G, eps)
--- simulator/kernel_test.py
+++ simulator/kernel_test.py
@@ -3,17 +3,17 @@ import numpy as np
 from simulator.space import Space
 from kernels.brute_force import calculate_accs_pp_wrap
 from kernels.octree import calc_accs_octree
-from kernels.numeric import calc_com_old, calc_com_wrap
+from kernels.numeric import calc_com_old, calc_com_wrap, calc_ke_old, calc_ke_wrap, calc_pe_old, calc_pe_wrap
 
 if __name__ == '__main__':
     def vel_func(pos_vec):
-        return np.array((0, 0, 0))
+        return np.array((1, 2, 3))
 
 
     def mass_func(pos_vec):
         return 1.0
 
-    n = 100000
+    n = 10000
     cube_length = 1.0
     G = 1.0
     eps = 1.0e-3
@@ -22,9 +22,10 @@ if __name__ == '__main__':
     space = Space()
     space.add_cuboid(n, np.array((0., 0., 0.)), cube_length, cube_length, cube_length, vel_func, mass_func)
 
-    x = calc_com_old(space.r, space.m)
-    y = calc_com_wrap(space.r, space.m)
-    assert np.allclose(x, y, atol=1e-15, rtol=1e-10)
+    #x = calc_pe_old(space.r, space.m, eps, G)
+    #y = calc_pe_wrap(space.r, space.m, G, eps)
+    #assert np.allclose(x, y)
+    #print(x, y)
 
     #accs2 = calculate_accs_pp_wrap(space.r, space.m, G, eps)
     #accs3 = calc_accs_octree(cube_length, 0., 0., 0., space.r, space.m, G, eps, theta)
