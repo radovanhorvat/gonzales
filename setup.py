0c8797b9289415c711ea729ea290b27b0219a78b
Radovan <radovan.horvat@gmail.com>
2020-11-15 17:52:56 +0100

    Comment out linker arg

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, Extension
 extensions = [Extension("brute_force", sources=[os.path.join('kernels', 'brute_force.pyx'),
                                                 os.path.join('kernels', 'cpp_src', 'brute_force.cpp')],
                         extra_compile_args=['/openmp'],
-                        extra_link_args=['/openmp'],
+                        #extra_link_args=['/openmp'],
                         include_dirs=['kernels', os.path.join('kernels', 'cpp_src'), np.get_include()],
                         language="c++"),
               Extension("octree", sources=[os.path.join('kernels', 'octree.pyx'),
@@ -18,11 +18,11 @@ extensions = [Extension("brute_force", sources=[os.path.join('kernels', 'brute_f
                                            os.path.join('kernels', 'cpp_src', 'octree.cpp')],
                         include_dirs=['kernels', os.path.join('kernels', 'cpp_src'), np.get_include()],
                         extra_compile_args=['/openmp'],
-                        extra_link_args=['/openmp'],
+                        #extra_link_args=['/openmp'],
                         language="c++"),
               Extension("numeric", sources=[os.path.join('kernels', 'numeric.pyx')],
                         extra_compile_args=['/openmp'],
-                        extra_link_args=['/openmp'],
+                        #extra_link_args=['/openmp'],
                         include_dirs=['kernels', np.get_include()]),
               ]
 
