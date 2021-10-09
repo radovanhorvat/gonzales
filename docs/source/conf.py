7b25296acbfa11ba2f212ae29d7e99aaf7d92af3
radovanhorvat <radovan.horvat@gmail.com>
2021-10-09 15:08:03 +0200

    Bump version to 0.1.2, add release docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Radovan Horvat'
 author = 'Radovan Horvat'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/source/release_history.rst
+++ docs/source/release_history.rst
@@ -11,3 +11,8 @@ First public release.
 .. rubric:: 0.1.1
 
 Add release history section to docs.
+
+.. rubric:: 0.1.2
+
+- fix major octree memory leak
+- fix brute force kernel bug
